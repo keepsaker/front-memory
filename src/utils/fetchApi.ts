935d459ba5ec6579d148fae30ce1275b31ac4d7d
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-08-06 20:31:59 +0900

    fix: baseUrl을 수정한다

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,7 +1,8 @@
-const baseURL = 'localhost:3000'; 
+const baseURL = 'http://localhost:3000'
 
 export async function fetchApi(url, options = {}) {
-
+  // 토큰을 가져와서 헤더에 넣어주는 경우 고려
+  // header에 Authorization: Bearer {token} 추가만 하면 됨
   const defaultOptions = {
     method: 'GET',
     headers: {
